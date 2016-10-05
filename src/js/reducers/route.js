45b318913cce4983eec736ed7a9df1105c32f5da
Veera <veera.k.sundar@gmail.com>
2016-10-04 20:09:56 -0700

    Make way points as Immutable List.

--- src/js/reducers/route.js
+++ src/js/reducers/route.js
@@ -1,12 +1,15 @@
 var ActionTypes = require('../actions/types');
 var util = require('../util');
+var Immutable = require('immutable');
+
 const route = (state = {}, action) => {
 
     switch (action.type) {
         case ActionTypes.ADD_ROUTE:
             return {
                 id: util.guid(),
-                name: 'New Route'
+                name: 'New Route',
+                wayPoints: Immutable.List()
             }
         default:
             return state;
