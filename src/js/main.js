ec3ee67c11675061d845243d337626988793034b
Veera <veera.k.sundar@gmail.com>
2016-10-08 18:39:44 -0700

    Integrate google maps library and initialize it upon app load.

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,17 @@
 require('../scss/base.scss');
 
+let React = require('react');
+let ReactDOM = require('react-dom');
+let App = require('./app');
+let domReady = require('domready');
+let GoogleMaps = require('google-maps');
 
-var React = require('react');
-var ReactDOM = require('react-dom');
-var App = require('./app');
+domReady(()=> {
+    GoogleMaps.LIBRARIES = ['places'];
+    GoogleMaps.KEY='AIzaSyAkpb6H_VzRih_zDtUnFJF9tgEG46S3hqU';
+    GoogleMaps.load((google)=> {
+        ReactDOM.render(<App mapService={google}/>,
+            document.getElementById('app'));
+    });
 
-
-ReactDOM.render(<App/>, document.getElementById('app'));
+});
