7f4e786f599d92d2bb7313e695c297b6c1f64b6e
Veera <veera.k.sundar@gmail.com>
2016-10-10 16:54:04 -0700

    Configure webpack to handle fonts and install needed dev dependencies.

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,13 @@ module.exports = {
             {
                 test: /\.scss$/,
                 loaders: ['style', 'css', 'sass']
+            },
+            {
+                test: /\.woff(2)?(\?[a-z0-9]+)?$/,
+                loader: "url-loader?limit=10000&mimetype=application/font-woff"
+            }, {
+                test: /\.(ttf|eot|svg)(\?[a-z0-9]+)?$/,
+                loader: "file-loader"
             }
         ]
     },
