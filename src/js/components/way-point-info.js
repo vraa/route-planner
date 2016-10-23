98134c73cc16ff8d0003c6ad28a89b51b9fd2449
Veera <veera.k.sundar@gmail.com>
2016-10-23 16:11:29 -0700

    Animate way point label upon hover.

--- src/js/components/way-point-info.js
+++ src/js/components/way-point-info.js
@@ -5,7 +5,7 @@ class WayPointInfo extends React.Component {
     render() {
         return (
             <div className="way-point-info">
-                <span className="distance"><i className="icon-directions_run"/> {this.props.distance}</span>
+                <span className="distance"><i className="icon-directions_car"/> {this.props.distance}</span>
                 <span className="duration"><i className="icon-clock"/> {this.props.duration}</span>
             </div>
         )
